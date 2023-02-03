f2edf79fd5ca7ff87e7edaf59d9a8dd58caf5b42
Jay <scottjay@msu.edu>
2023-02-02 23:38:10 -0500

    destructuring data object

--- src/App.js
+++ src/App.js
@@ -18,6 +18,7 @@ function App() {
       <section>
         {cards}
       </section>
+      <Weather />
     </>
   );
 }
--- src/components/Weather.js
+++ src/components/Weather.js
@@ -8,7 +8,12 @@ export default function Weather() {
     useEffect(()=> {
         fetch("http://api.weatherapi.com/v1/current.json?key=90ab25695d9f41ee81b20151233001&q=48823&aqi=no")
             .then(res => res.json())
-            .then(dataJS => setWeatherData(dataJS.current))
+            .then(dataJS => setWeatherData({
+                temp_c: dataJS.current.temp_c,
+                temp_f: dataJS.current.temp_f,
+                condition: dataJS.current.condition.text,
+                condition_icon: dataJS.current.condition.icon
+            }))
     }, [])
 
     console.log({weatherData})
